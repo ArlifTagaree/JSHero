2e10ba6ee44f9c4c07319ef711ee80940eece393
ArlifTagaree <liffism9@gmail.com>
2023-03-29 16:53:35 +0700

    passed

---
+++ 09-WhatIsX.js
@@ -0,0 +1,12 @@
+// Just practice this again.
+
+//-----------
+// Exercise
+// Which value does x have after execution of the following code?
+// function hello() {
+//   return 'Hi!';
+// }
+
+// let x = hello();
+
+'Hi!'
